5ed83b44b46a31f8b2cccfc18b00355ee53bb4dc
rao-rabi <rabirao1039@gmail.com>
2023-08-21 16:03:04 +0500

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ const Footer = () => {
     return(
         <>
         <div className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
-    ></div>
+    >
+        <ul className="links">
+            <a href="https://www.facebook.com"><li>Facebook</li></a>
+        </ul>
+    </div>
         </>
     )
 }
